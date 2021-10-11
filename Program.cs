be898e53ad3d61330cab59010ece6f6a406439f2
Eric <eric.michaud@clicetpuces.com>
2021-10-11 16:32:05 +0200

    last save before inverting client/server model

--- Program.cs
+++ Program.cs
@@ -3,174 +3,169 @@ using System.Net;
 using System;
 using System.Timers;
 using System.Threading;
+//using System.Text;
+//using System.Text.Json;
 using Newtonsoft.Json;
-using System.Text;
 using System.Collections.Generic;
+using System.IO;
 
 
 namespace ratiereSimulator
 {
 
 public class General {
-    private string version;
-    private sbyte temperature;
-    private sbyte temperaturemax;
-    private ushort speed;
-    private ushort speedmax;
-    private sbyte frame;
-    private uint idle;
-    private ulong totalcycle; 
-    public General(string _version,
-                   sbyte _temperature,
-                   sbyte _temperaturemax,
-                   ushort _speed,
-                   ushort _speedmax,
-                   sbyte _frame,
-                   uint _idle,
-                   ulong _totalcycle) {
-        this.version = _version;
-        this.temperature = _temperature;
-        this.temperaturemax = _temperaturemax;
-        this.speed = _speed;
-        this.speedmax = _speedmax;
-        this.frame = _frame;
-        this.idle = _idle;
-        this.totalcycle= _totalcycle;
+    public string Version { get ; set; }
+    public sbyte Temperature { get; set; }
+    public sbyte Temperaturemax { get; set; }
+    public ushort Speed { get; set; }
+    public ushort Speedmax { get; set; }
+    public sbyte Frame { get; set; }
+    public uint Idle { get; set; }
+    public ulong Totalcycle { get; set; }
+
+    public General(string version,
+                   sbyte temperature,
+                   sbyte temperaturemax,
+                   ushort speed,
+                   ushort speedmax,
+                   sbyte frame,
+                   uint idle,
+                   ulong totalcycle) {
+        this.Version = version;
+        this.Temperature = temperature;
+        this.Temperaturemax = temperaturemax;
+        this.Speed = speed;
+        this.Speedmax = speedmax;
+        this.Frame = frame;
+        this.Idle = idle;
+        this.Totalcycle= totalcycle;
     }
-    public string Version {
-        get => version;
-        set => version= Version;
-    }
-    public sbyte Temperature {
-        get => temperature;
-        set => temperature= Temperature;
-    }
+    
+
+   
     public void settemperature(sbyte Temperature) {
-        this.temperature = Temperature;
-        if (Temperature > this.temperaturemax) {
-            this.temperaturemax = Temperature; 
+        this.Temperature = Temperature;
+        if (Temperature > this.Temperaturemax) {
+            this.Temperaturemax = Temperature; 
         }
     }
     public void setspeed(ushort Speed) {
-        this.speed = Speed;
-        if (Speed > this.speedmax) {
-            this.speedmax = Speed; 
+        this.Speed = Speed;
+        if (Speed > this.Speedmax) {
+            this.Speedmax = Speed; 
         }
     }
 
-    public sbyte Temperaturemax {
-        get => temperaturemax;
-        set => temperaturemax= Temperaturemax;
-    }
-    public ushort Speed {
-        get => speed;
-        set => speed= Speed;
-    }
-    public ushort Speedmax {
-        get => speedmax;
-        set => speedmax= Speedmax;
-    }
-    public sbyte Frame {
-        get => frame;
-        set => frame= Frame;
-    }
-    public uint Idle {
-        get => idle;
-        set => idle= Idle;
-    }
-    public ulong Totalcycle {
-        get => totalcycle;
-        set => totalcycle = Totalcycle;
-    }
 
 } // General
 
 
 public class TableV {
-   
-    private uint[] speedTable;  
-
+    public uint[] SpeedTable { get ; set ; }
     public TableV (int size){
-        speedTable=new uint[size];
+        SpeedTable=new uint[size];
     }    
 
-    public uint[] SpeedTable {
-        get => speedTable;
-        set => speedTable= SpeedTable;
-    }
+//    [JsonConstructor]
+    public TableV (uint[] speedTable){
+        SpeedTable=speedTable;
+    } 
+
 } // TableV
 
 public class TableT {
    
-    private uint[] tempTable;  
-
+    public uint[] TempTable { get; set; }
     public TableT (int size){
-        tempTable=new uint[size];
+        TempTable=new uint[size];
     }    
 
-    public uint[] TempTable {
-        get => tempTable;
-        set => tempTable= TempTable;
-    }
 } // TableT
 
 public class TableL {
-   
-    private uint[] lame;  
-
+    public uint[] Lame { get; set;}
     public TableL (int size){
-        lame=new uint[size];
+        Lame=new uint[size];
     }    
+       
 
-    public uint[] Lame {
-        get => lame;
-        set => lame= Lame;
-    }
 } // TableL
 
 
 public class Report_data {
-    private General general;          /* version de la structure */
-    private TableV speedTable;  /* temps passé en marche par vitesse */
-    private TableT tempTable;    /* temps de fonctionnement par degres */
-    private ulong picks_counter;     /* nombre totale de duites */
-    private TableL lameTable;    /* nombre de cycles par lames */
-    
-   public Report_data(){
-        this.general = new General("2",0,0,0,0,28,0,0);
-        this.speedTable = new TableV(50);
-        this.tempTable = new TableT(50);
-        this.picks_counter = 0;
-        this.lameTable = new TableL(28); 
-    }
-    public General General {
-        get => general;
-        set => general=General;
-    }
-    public void settemperature(sbyte temperature) {
-        this.general.settemperature(temperature);   
-    }
-     public void setspeed(ushort Speed) {
-        this.general.setspeed(Speed);   
+
+    public General General { get; set;}
+
+    public TableV TableV { /* temps passé en marche par vitesse */
+        get;
+        set;
     }
 
-    public ulong Picks_counter {
-        get => picks_counter;
-        set => picks_counter=Picks_counter;
+    public TableT TableT {  /* temps de fonctionnement par degres */
+        get;
+        set;
     }
-    public TableV SpeedTable {
-        get => speedTable;
-        set => speedTable=SpeedTable;
+   public ulong Picks_counter { get; set;}
+   public TableL TableL {  /* temps de fonctionnement par degres */
+        get;
+        set;
     }
-    public TableT TempTable {
-        get => tempTable;
-        set => tempTable=TempTable;
-    }    
-    public TableL LameTable {
-        get => lameTable;
-        set => lameTable=LameTable;
-    }    
     
+   public Report_data(){
+      
+        this.General = new General("2",0,0,0,0,28,0,0);
+        this.TableV = new TableV(50);
+        this.TableT = new TableT(50);
+        this.Picks_counter = 0;
+        this.TableL = new TableL(28); 
+    }
+
+     
+    public void settemperature(sbyte temperature) {
+        this.General.settemperature(temperature);   
+    }
+
+     public void setspeed(ushort Speed) {
+        this.General.setspeed(Speed);   
+    }
+   
+    public Boolean Save(string fileName) {
+
+       // var options = new JsonSerializerOptions { WriteIndented = false };
+       //JsonSerializer.Serialize(this, options);
+        string json = JsonConvert.SerializeObject(this);
+        Console.WriteLine("saving report to "+ fileName);
+        Console.WriteLine(json);
+        File.WriteAllText(@fileName,json);
+        return true;    
+    }
+    public Boolean Restore(string fileName) {
+        if (File.Exists(fileName)) {
+            Console.WriteLine("Restore report from "+ fileName);
+            string readText = File.ReadAllText(@fileName);      
+            Console.WriteLine(readText);
+            
+            //readText ="{\"Version\":\"2\",\"Temperature\":61,\"Temperaturemax\":61,\"Speed\":745,\"Speedmax\":745,\"Frame\":28,\"Idle\":0,\"Totalcycle\":0}";
+              
+            //var read_report = JsonSerializer.Deserialize<Report_data>(readText);
+            Report_data read_report = JsonConvert.DeserializeObject<Report_data>(readText);
+            Console.WriteLine("Deserialized done");          
+     
+            this.General = read_report.General;
+            this.TableV = read_report.TableV;
+            this.TableT = read_report.TableT;
+            this.Picks_counter = read_report.Picks_counter;
+            this.TableL = read_report.TableL;
+        }
+        
+        return true;    
+    }
+
+
+
+
+
+
 } //Report_Data
     
 public class Work {
@@ -213,14 +208,15 @@ public class Work {
 
      
         if (this.running) {
-            Report.SpeedTable.SpeedTable[indiceSpeed] += nbcoup;
-            Report.TempTable.TempTable[indiceTemp] += nbcoup;
+            Report.TableV.SpeedTable[indiceSpeed] += nbcoup;
+            Report.TableT.TempTable[indiceTemp] += nbcoup;
             Random rnd = new Random();
             for (short i=0;i<28;i++) {
-                Report.LameTable.Lame[i] += (uint)rnd.Next((int)nbcoup);   
+                Report.TableL.Lame[i] += (uint)rnd.Next((int)nbcoup);   
             }
 
             Report.Picks_counter += nbcoup; 
+            Report.General.Totalcycle = Report.Picks_counter;
         }    
         else {
             Report.General.Idle += this.periode;
@@ -253,7 +249,10 @@ public class Ratiere {
         set => work= Work;
     }    
 
-
+    public int Port {
+        get => port;
+        set => port=Port;
+    }
 
 
 
@@ -284,20 +283,25 @@ public class Ratiere {
 
         ongoing = work.Interval(report);
 
-        var json = JsonConvert.SerializeObject(this.Report.General, Formatting.None)+ "\r\n";
+        string json = JsonConvert.SerializeObject(this.Report.General)+ "\r\n";
+        // var json = JsonSerializer.Serialize(this.Report.General)+ "\r\n";
+ 
         this.server.SendAll(json);
 
       
         //if (this.stream!=null) this.stream.Write(byteArray,0,byteArray.Length);     
         
-        json = JsonConvert.SerializeObject(this.Report.SpeedTable, Formatting.None)+ "\r\n";
+        json = JsonConvert.SerializeObject(this.Report.TableV)+ "\r\n";
+        //  json = JsonSerializer.Serialize(this.Report.TableV)+ "\r\n";
         this.server.SendAll(json);
     
         
-        json = JsonConvert.SerializeObject(this.Report.TempTable, Formatting.None)+ "\r\n";
+        json = JsonConvert.SerializeObject(this.Report.TableT)+ "\r\n";
+        // json = JsonSerializer.Serialize(this.Report.TableT)+ "\r\n";
         this.server.SendAll(json);
 
-        json = JsonConvert.SerializeObject(this.Report.LameTable, Formatting.None)+ "\r\n";
+        json = JsonConvert.SerializeObject(this.Report.TableL)+ "\r\n";
+        // json = JsonSerializer.Serialize(this.Report.TableL)+ "\r\n";
         this.server.SendAll(json);
     
         if (!ongoing) {
@@ -335,7 +339,9 @@ class Program {
 
         for (int i=0;i<NBRATIERE;i++) {
             Console.WriteLine("Create new ratiere {0}:", i);
-            listRatiere.Add(new Ratiere(ADDRESS,PORT+i));
+            Ratiere ratiere = new Ratiere(ADDRESS,PORT+i);
+            ratiere.Report.Restore(".\\report_" + ratiere.Port.ToString());
+            listRatiere.Add(ratiere);
         }
 
         aTimer = new System.Timers.Timer();
@@ -365,6 +371,7 @@ class Program {
         
         foreach (Ratiere ratiere in listratiere) {
             ratiere.Interval(); 
+            ratiere.Report.Save(".\\report_" + ratiere.Port.ToString());
         } 
         
     }
